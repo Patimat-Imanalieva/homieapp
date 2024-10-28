fe2e94f2afa097d5aa70ba2d2248d0d1c00e5a5b
imnlv <pimanalieva_1@edu.hse.ru>
2024-10-28 17:18:18 +0300

    Initial Commit

---
+++ homieapp/homieappApp.swift
@@ -0,0 +1,17 @@
+//
+//  homieappApp.swift
+//  homieapp
+//
+//  Created by Patima Imanalieva on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct homieappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ homieappTests/homieappTests.swift
@@ -0,0 +1,17 @@
+//
+//  homieappTests.swift
+//  homieappTests
+//
+//  Created by Patima Imanalieva on 28.10.2024.
+//
+
+import Testing
+@testable import homieapp
+
+struct homieappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
